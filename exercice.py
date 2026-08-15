0f6bc818b85166e54cc373137829759906bab234
bouchta65 <bouchtamohamed01@gmail.com>
2026-08-15 22:07:37 +0100

    exerice test

---
+++ exercice.py
@@ -0,0 +1,39 @@
+print("My name is mohamed")
+
+
+#Numbers :
+
+
+# int   (1 2 3 4 5 6)
+# float (1.5 2.3 6.8)
+
+
+
+# #text 
+
+# str 
+
+
+# #Booleans :
+# bool
+
+
+age: int = 18
+name = "mohammed"
+
+name: str = "mohamed"
+
+is_student: bool = False
+
+print("age")
+print(name)
+
+
+print("give me the first number")
+
+first_number = int(input("give me the first number: "))
+number_2 = int(input("give me the number 2: "))
+
+result = first_number + number_2
+print(result)
+
