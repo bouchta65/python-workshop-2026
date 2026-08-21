5aae920b238cc8332e03f7e15811876694fa4dc9
bouchta65 <bouchtamohamed01@gmail.com>
2026-08-21 20:02:38 +0100

    local update

--- exercice.py
+++ exercice.py
@@ -1,39 +1,118 @@
-print("My name is mohamed")
+# print("My name is mohamed")
 
 
-#Numbers :
+# #Numbers :
 
 
-# int   (1 2 3 4 5 6)
-# float (1.5 2.3 6.8)
+# # int   (1 2 3 4 5 6)
+# # float (1.5 2.3 6.8)
 
 
 
-# #text 
+# # #text 
 
-# str 
-
-
-# #Booleans :
-# bool
+# # str 
 
 
-age: int = 18
-name = "mohammed"
-
-name: str = "mohamed"
-
-is_student: bool = False
-
-print("age")
-print(name)
+# # #Booleans :
+# # bool
 
 
-print("give me the first number")
+# age: int = 18
+# name = "mohammed"
 
-first_number = int(input("give me the first number: "))
-number_2 = int(input("give me the number 2: "))
+# name: str = "mohamed"
 
-result = first_number + number_2
-print(result)
+# is_student: bool = False
+
+# print("age")
+# print(name)
+
+
+# print("give me the first number")
+
+# first_number = int(input("give me the first number: "))
+# number_2 = int(input("give me the number 2: "))
+
+# result = first_number + number_2
+# print(result)
+
+
+
+
+# time1 = input("Enter the first time: ")
+# time2 = input("Enter the second time: ")
+
+# if time1 < time2:
+#     print("The first time comes before the second.")
+# elif time2 < time1:
+#     print("The second time comes before the first.")
+# else:
+#     print("They are the same time.")
+
+
+
+
+
+
+
+
+
+
+
+# print("my name is mohamed")
+
+
+# for i in range(100, 0,-2):
+#     print("my name is mohamed")
+
+# x = 15
+# i = 20
+# j = 15
+
+# x = 0
+
+# table = [15,20,100,1,14,25,17,80,3,30,5,99,80,45]
+
+# for i in range(14):
+#     for j in range(14):
+#         if table[i] < table[j]:
+#             x = table[i]
+#             table[i] = table[j]
+#             table[j] = x
+# print(table)
+
+
+    # for j in range(14):
+    #     print(table[i],table[j])
+
+
+
+
+
+
+# while True:
+#     i=0
+#     i = i+1
+#     print("my name is mohamed", i)
+
+# for i=0,i<=100,i+2:
+#     print(i)
+
+
+
+
+
+
+
+# table = [1,2,3,4,5,6,7,8,9,10]
+j = 0
+
+1,2,3,4,5,6,7,8,9,10
+ 
+for i in range(10):
+    if j<i:
+        print(i*j)
+    j= i+1
+    
 
